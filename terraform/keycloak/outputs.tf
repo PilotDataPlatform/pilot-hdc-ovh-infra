7add0dc37f42c7dd31cf4eeb64bebac1529c82e7
Antonio Cascais <acascais@indocresearch.org>
2026-01-30 12:16:05 +0100

    feat: Add kong Keycloak client for auth service user management
    Confidential client with service account roles required for token
    introspection and user management (manage-realm, manage-users).
    Includes protocol mappers matching CSCS staging config.

--- terraform/keycloak/outputs.tf
+++ terraform/keycloak/outputs.tf
@@ -5,3 +5,12 @@ output "realm_id" {
 output "react_app_client_id" {
   value = keycloak_openid_client.react_app.client_id
 }
+
+output "kong_client_id" {
+  value = keycloak_openid_client.kong.client_id
+}
+
+output "kong_client_secret" {
+  value     = keycloak_openid_client.kong.client_secret
+  sensitive = true
+}
